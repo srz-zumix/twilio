411790242f8425e2f18d9c1c19d381ae32b86c0b
srz_zumix <zumix.cpp@gmail.com>
2015-03-11 12:36:00 +0900

    update

--- gather.php
+++ gather.php
@@ -1,16 +1,16 @@
 <?php
-require '/twilio-sample-123/twilio-php/Services/Twilio.php';
+require 'twilio-php/Services/Twilio.php';
 
 $response = new Services_Twilio_Twiml();
 if( isset($_REQUEST['Digits']) ) {
   $input = $_REQUEST['Digits'];
   switch($input) {
-  case '1':
-    $gather = $response->gather(array('numDigits' => 1, 'timeout' => '10'));
-    $gather->say('1を押したな', array('language' => 'ja-jp'));
-    break;
   case '9':
     $response->say('さようなら', array('language' => 'ja-jp'));
+    break;
+  default:
+    $gather = $response->gather(array('numDigits' => 1, 'timeout' => '10'));
+    $gather->say($input . 'を押したな', array('language' => 'ja-jp'));
     break;
   }
 } else {
